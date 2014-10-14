fee7ac64fff922b642dddde801d5f86ff69e33cb
Armani Black <black747@umn.edu>
2014-10-13 22:11:47 -0500

    I finished it!

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -4,9 +4,14 @@ Public Class Form1
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Dim st As String = txtInput.Text
+        Dim result As String = ""
+        Dim i As Integer
+        For i = st.Length - 1 To 0 Step -1
+            result = result & st.Chars(i)
 
-        ' Please write your code here!
+            ' Please write your code here!
+        Next
 
-        txtOutput.Text = "Not implemented yet!"
+        txtOutput.Text = result
     End Sub
 End Class
